f872b23a3e25a796563992b638356b82977d963d
cdmiller540 <cdmiller540@ufl.edu>
2015-09-01 17:13:45 -0400

    Initial Commit

---
+++ cis4930_horoscope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cis4930_horoscope
+//
+//  Created by Cody Miller on 9/1/15.
+//  Copyright (c) 2015 Cody Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ cis4930_horoscopeTests/cis4930_horoscopeTests.swift
@@ -0,0 +1,36 @@
+//
+//  cis4930_horoscopeTests.swift
+//  cis4930_horoscopeTests
+//
+//  Created by Cody Miller on 9/1/15.
+//  Copyright (c) 2015 Cody Miller. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class cis4930_horoscopeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
